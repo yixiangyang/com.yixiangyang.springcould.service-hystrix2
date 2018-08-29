a7c76ec289690493da868d4d5bcb4ace25ec48f7
xiangyang <15138778469@163.com>
2018-08-29 22:21:31 +0800

    update

---
+++ src/main/java/com/yixiangyang/springcould/hystrix2/application/Application.java
@@ -0,0 +1,12 @@
+package com.yixiangyang.springcould.hystrix2.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
